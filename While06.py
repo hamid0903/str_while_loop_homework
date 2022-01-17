025985c93a88217f61bf5138400fc4f9fda7f879
Hamid Kamalov <hamid_kamalov@mail.ru>
2022-01-17 22:14:52 +0500

    while06-1

--- While06.py
+++ While06.py
@@ -7,4 +7,14 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    idx = 0
+    cons = 0
+    while idx < len(s):
+        if s[idx].isalpha():
+            if s[idx]!="a" or s[idx]!="A" or s[idx]!="e" or s[idx]!="E" or s[idx]!="i" or s[idx]!="I" or s[idx]!="o" or s[idx]!="O" or s[idx]!="u"or s[idx]!="U":
+                cons+=1
+            else:
+                cons+=0
+        idx += 1
+    return cons
+print(main("abcdefghklmnopqrstuvwxyz"))
