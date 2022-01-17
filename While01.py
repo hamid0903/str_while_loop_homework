6f1c879aec5bf33af4e135bde66d2a17faea3275
Hamid Kamalov <hamid_kamalov@mail.ru>
2022-01-17 22:06:41 +0500

    while01-1

--- While01.py
+++ While01.py
@@ -6,4 +6,11 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    idx=0
+    numb=0
+    while idx<len(s):
+        if s[idx].isdigit():
+            numb+=1
+        idx+=1
+    return numb
+print(main("20abc21"))
