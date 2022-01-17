830f0a8f1fde97337268403bef0b5f5149a0084d
Hamid Kamalov <hamid_kamalov@mail.ru>
2022-01-17 22:16:06 +0500

    while09-1

--- While09.py
+++ While09.py
@@ -6,4 +6,11 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    idx=0
+    numb=0
+    while idx<len(s):
+        if s[idx].isdigit():
+            numb+=int(s[idx])
+        idx+=1
+    return numb
+print(main("10110110"))
